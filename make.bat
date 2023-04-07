84ca656591fb138dd3ee5aa54e0dffa2350335c7
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-04-07 14:44:28 +0200

    Change calculation of LSB/MSB

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 xas99.py src/main-memory.a99 -i -q -R -L trapdoor.lst -o bin/main
-xas99.py src/cartridge-banks.a99 -B -q -R -o bin/banks.bin
+xas99.py src/cartridge-banks.a99 -B -q -R -L cartridge.lst -o bin/banks.bin
 
 java -jar tools/ea5tocart.jar bin\main "TRAP DOOR"
 
