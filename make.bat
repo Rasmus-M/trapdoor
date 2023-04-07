a0052d365df1ed39d63cd6221c6baf3c7e289fc3
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-04-07 15:38:03 +0200

    Fix level 2

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 xas99.py src/main-memory.a99 -i -q -R -L trapdoor.lst -o bin/main
 xas99.py src/cartridge-banks.a99 -B -q -R -L cartridge.lst -o bin/banks.bin
 
-java -jar tools/ea5tocart.jar bin\main "TRAP DOOR"
+java -jar tools/ea5tocart.jar bin\main "THROUGH THE TRAPDOOR"
 
 copy /b bin\main8.bin ^
     + bin\banks.bin ^
