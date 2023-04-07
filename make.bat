21e4aa2fcbab7b27f8f7ac2728c7eda4986bfeb1
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-04-07 11:18:01 +0200

    Cartridge almost working

--- make.bat
+++ make.bat
@@ -1,1 +1,8 @@
-xas99.py src/trap-door-main.a99 -q -R -L trapdoor.lst -o main.obj
+xas99.py src/main-memory.a99 -i -q -R -L trapdoor.lst -o bin/main
+xas99.py src/cartridge-banks.a99 -B -q -R -o bin/banks.bin
+
+java -jar tools/ea5tocart.jar bin\main "TRAP DOOR"
+
+copy /b bin\main8.bin ^
+    + bin\banks.bin ^
+    trapdoor8.bin
