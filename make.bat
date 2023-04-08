a1abf1f28435bdfbd120c161143aa4fe921abba0
Rasmus Moustgaard <rasmus.moustgaard@gmail.com>
2023-04-08 20:32:16 +0200

    Fix bug update timer

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-xas99.py src/main-memory.a99 -i -q -R -L trapdoor.lst -o bin/main
+xas99.py src/main-memory.a99 -i -q -R -L trapdoor.lst -E symbols.txt -o bin/main
 xas99.py src/cartridge-banks.a99 -B -q -R -o bin/banks.bin
 
 java -jar tools/ea5tocart.jar bin\main "THROUGH THE TRAPDOOR"
